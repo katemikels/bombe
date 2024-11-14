93668fab7c043e71909937960e74c78c4b729f13
Kate Mikels <kate_mikels@taylor.edu>
2024-11-14 15:16:03 -0500

    added scanln to main to read in the ciphertext

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package bombe
+package main
+
+import "fmt"
+
+func main() {
+	var cipherText string
+	n, err := fmt.Scanln(&cipherText)
+	fmt.Println("Input read:")
+	fmt.Println(n, err)
+	return
+}
