83914be58523358f003394207df2aa98bb048339
Kate Mikels <kate_mikels@taylor.edu>
2024-11-25 22:08:02 -0500

    possible encryptChar and stepRotors working?

--- main.go
+++ main.go
@@ -7,8 +7,49 @@ import (
 
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// create an array of all possible rotor positions
+// TODO confirm these are the right turnovers??? From OtherBombCode/rotors.txt. Also assuming moving B->R is what causes turnover in rotor I
+var rotorInfo = map[string][]string{
+	"I":   {"EKMFLGDQVZNTOWYHXUSPAIBRCJ", "R"},
+	"II":  {"AJDKSIRUXBLHWTMCQGZNPYFVOE", "F"},
+	"III": {"BDFHJLCPRTXVZNYEIWGAKMUSQO", "W"},
+	"IV":  {"ESOVPZJAYQUIRHXLNFTGKDCMWB", "K"},
+	"V":   {"VZBRGITYUPSDNHLXAWMJQOFECK", "A"},
+}
 
+type rotorStruct struct {
+	name          string
+	letters       string
+	turnover      string
+	wiring        map[rune]rune
+	wiringInverse map[rune]rune
+}
+
+// practically to get where the letter is on a rotator. "Where is `pos` in my `str`?
+func index(pos rune, str string) rune {
+	for i, c := range str {
+		if c == pos {
+			return rune(i)
+		}
+	}
+	return -1
+}
+
+func makeWiring(r *rotorStruct) {
+	wiring := make(map[rune]rune)
+	wiringInverse := make(map[rune]rune)
+
+	for i, letter := range r.letters {
+		in := rune(i)
+		out := index(letter, alphabet)
+		wiring[in] = out
+		wiringInverse[out] = in
+	}
+	r.wiring = wiring
+	r.wiringInverse = wiringInverse
+	return
+}
+
+// create an array of all possible rotor positions
 func rotorPositions() []string {
 	positions := make([]string, 0)
 	for i := 0; i < len(alphabet); i++ {
@@ -20,22 +61,6 @@ func rotorPositions() []string {
 		}
 	}
 	return positions
-}
-
-// sketch of how to do the reflector.
-// a switch statement would take a lot more code and writing,
-// do we want to use runes? Otherwise we'd be doing a lot of
-// conversion to strings on line 15 for alph[i]
-func reflector(in rune) rune {
-	alph := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	ukwb := "YRUHQSLDPXNGOKMIEBFZCWVJAT"
-
-	for i := 0; i < 26; i++ {
-		if rune(alph[i]) == in {
-			return rune(ukwb[i])
-		}
-	}
-	return '0' // not found
 }
 
 // this is not the most efficient way, but it works
@@ -92,9 +117,99 @@ func createMenu(crib string, cipherCrib string) map[rune]map[rune]int {
 	return menu
 }
 
+func stepRotors(rotors []rotorStruct, pos []rune) []rune {
+	// rotors: list of 3 rotor structs, the left, middle, and right rotor names respectively
+	// pos: the current letter visible on each rotor. Ex: "ABC" means left rotor is on A
+
+	// use inverse to loop right rotor first (backwards from the written order)
+	var rotorsInverse []rotorStruct
+	for i := len(rotors) - 1; i >= 0; i-- {
+		rotorsInverse = append(rotorsInverse, rotors[i])
+	}
+
+	var posInverse []rune
+	for i := len(pos) - 1; i >= 0; i-- {
+		posInverse = append(posInverse, pos[i])
+	}
+
+	// stepping rotors only advances the right rotor, unless it crosses a turning point
+	for i, r := range rotorsInverse {
+		currentPos := index(posInverse[i], r.letters)
+		updatePos := (currentPos + 1) % 26
+		posInverse[i] = rune(r.letters[updatePos])
+
+		if rune(r.turnover[0]) != posInverse[i] {
+			break
+		}
+	}
+	var newPos []rune
+	newPos = append(newPos, posInverse[2], posInverse[1], posInverse[0])
+	return newPos
+}
+
+// this makes me cringe. but yes. it does work.
+func encryptChar(char rune, rotors []rotorStruct, pos []rune) (rune, []rune) {
+	// TODO this does not use the plugboard in the encryption
+
+	// before a character goes through, the rotators step
+	pos = stepRotors(rotors, pos)
+
+	// use inverse to loop right rotor first (backwards from the written order)
+	var rotorsInverse []rotorStruct
+	for i := len(rotors) - 1; i >= 0; i-- {
+		rotorsInverse = append(rotorsInverse, rotors[i])
+	}
+
+	var posInverse []rune
+	for i := len(pos) - 1; i >= 0; i-- {
+		posInverse = append(posInverse, pos[i])
+	}
+
+	// get the rotor offsets -- how many clicks forward has the rotor gone?
+	// How much offset is the rotor starting at after the beginning of the string?
+	var offsets []rune
+	for i := 0; i < len(pos); i++ {
+		offset := index(pos[i], rotors[i].letters)
+		offsets = append(offsets, offset)
+	}
+
+	var offsetsInverse []rune
+	for i := len(offsets) - 1; i >= 0; i-- {
+		offsetsInverse = append(offsetsInverse, offsets[i])
+	}
+
+	// value of the char as an offset, not ASCII value
+	charIdx := char - 65
+
+	// through rotors right -> left
+	for i, rotor := range rotorsInverse {
+		// I gotta be honest, I copied this idea straight from enigma.py
+		// I never would have thought it out like this without it.
+		charIdx = (charIdx + offsetsInverse[i]) % 26 // adjust for rotor offset
+		charIdx = rotor.wiring[charIdx]              // wiring gives us the alphabet idx of the char on the rotor
+		//charIdx = (charIdx - offsetsInverse[i]) % 26 // why does enigma.py have this line??
+	}
+
+	// reflector
+	ukwb := "YRUHQSLDPXNGOKMIEBFZCWVJAT"     // reflector string
+	reflectorChar := rune(ukwb[charIdx])     // the character in the reflector at our index
+	charIdx = index(reflectorChar, alphabet) // where that character is in the alphabet (the wiring)
+
+	// opposite direction through the rotors
+	for i, rotor := range rotors {
+		charIdx = (charIdx + offsets[i]) % 26 // adjust for rotor offset
+		charIdx = rotor.wiring[charIdx]
+		//charIdx = (charIdx - offsets[i]) % 26 // back out of rotor offset... why???
+	}
+
+	char = rune(alphabet[charIdx]) // back to ASCII
+	return char, pos
+}
+
 // DFS to find paths in the menu
 func searchForPaths(letter rune, menu map[rune]map[rune]int, current rune, path []rune, paths *[]string) {
 	// for all the letters associated with the current letter
+	// TODO rename `new` bc new is a keyword
 	for new := range menu[current] {
 		// if the new letter is the same as the start (and the path is longer than two) then we found a loop
 		if new == letter && len(path) > 2 {
@@ -122,7 +237,19 @@ func searchForPaths(letter rune, menu map[rune]map[rune]int, current rune, path 
 	}
 }
 
-func runBombe(paths string, inputLetter string, menu map[rune]map[rune]int) {
+func runBombe(paths []string, inputLetter string, menu map[rune]map[rune]int) {
+	// hardcoded rotors?
+	var rotors []rotorStruct
+	rotorNames := []string{"I", "IV", "III"}
+	for _, name := range rotorNames {
+		var r rotorStruct
+		r.name = name
+		r.letters = rotorInfo[name][0]
+		r.turnover = rotorInfo[name][1]
+		makeWiring(&r)
+		rotors = append(rotors, r)
+	}
+
 	// - check all rotator positions (which ones, what order, starting order)
 	// this does not currently consider the different possible rotors
 	rotorPositions := rotorPositions()
@@ -133,6 +260,17 @@ func runBombe(paths string, inputLetter string, menu map[rune]map[rune]int) {
 			// - for all paths, break at contradictions (remember them for shortcuts later?)
 			for _, path := range paths {
 				// - send through enigma rotators/reflector -> write the rotators to step through to the index
+
+				// example code for stepping rotors
+				var position = []rune{'B', 'A', 'I'}
+				fmt.Println(position)
+				//newPosition := stepRotors(rotors, position)
+				//fmt.Println(newPosition) // to make newPosition used
+
+				// example code for encrypting char -- I thiiiiiiiinnkkkk the logic is right?? I'm not really sure. But if my understanding is right, then it works.
+				char := 'C'
+				char, position = encryptChar(char, rotors, position)
+
 				// - no contradictions, is a possibility (check other paths?)
 				// add all possibilities to list of all possibilities for all possible cribs
 
@@ -195,6 +333,7 @@ func main() {
 		if len(paths) != 0 {
 			// - decide on input letter (start of loop path)
 			//runBombe(paths, inputLetter, menu) // update as parameters change and given output
+			runBombe(paths, "A", menu)
 		}
 
 		// after the loop, checkCipherText() to check our possibilities against the entire message
@@ -205,6 +344,7 @@ func main() {
 		// then we found the plugboard settings and the rotator arrangement at the same time
 		// (previous lines will continue the loop before getting this far)
 		start = cribStart + 1
+		break
 	}
 
 	// go through the cipher text and decode using the plugboard/rotator settings/reflector
